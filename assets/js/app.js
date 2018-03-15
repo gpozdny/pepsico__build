eeead34007bab5d35dc4c672eb97673dc957f994
Gennady Yudin <soulmatesneverdie92@gmail.com>
2018-03-15 12:40:11 +0300

    'filter'

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,26 @@
+$( document ).ready(function() {
+    
+    // filter dropdown
+    $('.list__filter').click(function(){
 
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIiXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
+        let dropdown = $('.filter__dropdown');
+        let filter = $('.list__filter');
+
+        dropdown.toggleClass("filter__dropdown--active");
+        filter.toggleClass("list__filter--active");
+        
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIkKCBkb2N1bWVudCApLnJlYWR5KGZ1bmN0aW9uKCkge1xyXG4gICAgXHJcbiAgICAvLyBmaWx0ZXIgZHJvcGRvd25cclxuICAgICQoJy5saXN0X19maWx0ZXInKS5jbGljayhmdW5jdGlvbigpe1xyXG5cclxuICAgICAgICBsZXQgZHJvcGRvd24gPSAkKCcuZmlsdGVyX19kcm9wZG93bicpO1xyXG4gICAgICAgIGxldCBmaWx0ZXIgPSAkKCcubGlzdF9fZmlsdGVyJyk7XHJcblxyXG4gICAgICAgIGRyb3Bkb3duLnRvZ2dsZUNsYXNzKFwiZmlsdGVyX19kcm9wZG93bi0tYWN0aXZlXCIpO1xyXG4gICAgICAgIGZpbHRlci50b2dnbGVDbGFzcyhcImxpc3RfX2ZpbHRlci0tYWN0aXZlXCIpO1xyXG4gICAgICAgIFxyXG4gICAgfSlcclxuXHJcblxyXG5cclxuXHJcblxyXG5cclxuXHJcblxyXG5cclxuXHJcblxyXG5cclxufSk7Il0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
